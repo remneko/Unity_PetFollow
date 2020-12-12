16eb6b9db84b556f216888367fa8d149406f0426
remneko <73380357+remneko@users.noreply.github.com>
2020-12-12 13:54:37 +0800

    PUSH

---
+++ UnityPetFollow/Assets/Scripts/follow.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+public class follow : MonoBehaviour
+{
+    [Header("目標")]
+    public Transform target;
+    [Header("追蹤速度")]
+    public float speed = 1.5F;
+    private void followperson()
+    {
+        Vector3 p1 = transform.position;
+        Vector3 p2 = target.position;
+        p1 = Vector3.Lerp(p1, p2, speed*Time.deltaTime);
+
+        transform.position = p1;
+
+    }
+
+    public void LateUpdate()
+    {
+        followperson();
+    }
+}
